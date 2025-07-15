cbd752de80b641d31444bfa1874a0865bd4c8b58
tatsumaru20516 <rixen71@gmail.com>
2025-07-15 16:11:08 +0900

    feat:画面遷移時、全てのポケモンデータを取得

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
 import './App.css';
+import { getAllPokemon } from './utils/pokemon';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const initialURL = "https://pokeapi.co/api/v2/pokemon" //ポケモンAPIの初期URL
+
+
+  useEffect(() => {
+    //asyncアロー関数を定義し、関数をfetchPokemonData変数に代入
+    const fetchPokemonData = async () => { 
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
+    };
+    fetchPokemonData(); //fetchPokemonData関数を呼び出す
+  }, []);
+
+
+  return (<div className="App"></div>);
 }
 
 export default App;

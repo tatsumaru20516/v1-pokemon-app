7276b10d8a431bfd3eb17e1b762ab699df2a3060
tatsumaru20516 <rixen71@gmail.com>
2025-07-15 16:27:37 +0900

    feat:loadingを追加

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon" //ポケモンAPIの初期URL
-
+  const [loading, setLoading] = useState(true); //ローディング状態を管理するためのuseStateフック
 
   useEffect(() => {
     //asyncアロー関数を定義し、関数をfetchPokemonData変数に代入
-    const fetchPokemonData = async () => { 
+    const fetchPokemonData = async () => {
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
     fetchPokemonData(); //fetchPokemonData関数を呼び出す
   }, []);
 
 
-  return (<div className="App"></div>);
+  return (
+    <div className="App">
+      {
+        loading
+          ? <h1>ロード中・・・</h1>
+          : <h1>ロード完了</h1>
+      }
+    </div>
+  );
+  
 }
 
 export default App;

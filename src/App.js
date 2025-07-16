4616c2221d27a258ba5b4a5a89154cd203875746
tatsumaru20516 <rixen71@gmail.com>
2025-07-16 15:07:21 +0900

    feat:Navbarを追加

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon, getPokemon } from './utils/pokemon';
 import Card from './components/Card/Card';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon" //ポケモンAPIの初期URL
@@ -36,19 +37,22 @@ function App() {
 
   // Appコンポーネントとして、JSXを返すメイン部分
   return (
-    <div className="App">
-      {
-        loading ? (
-          <h1>ロード中・・・</h1>
-        ) : (
-          <div className="pokemonCardContainer">
-            {pokemonData.map((pokemon, i) => {
-              return <Card key={i} pokemon={pokemon} />;
-            })}
-          </div>
-        )
-      }
-    </div>
+    <>
+      <Navbar />
+      <div className="App">
+        {
+          loading ? (
+            <h1>ロード中・・・</h1>
+          ) : (
+            <div className="pokemonCardContainer">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />;
+              })}
+            </div>
+          )
+        }
+      </div>
+    </>
   );
 
 }
